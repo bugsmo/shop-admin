ed02cd21e35bed4cef778437892447246527f570
pistachio0101 <18550039021@163.com>
2022-09-29 18:12:44 +0800

    添加环境配置

---
+++ .history/src/axios_20220929181232.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { getToken } from '~/composables/auth.js'
+import { toast } from "./composables/utils";
+import store from '~/store'
+
+const service = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API
+})
+
+// 添加请求拦截器
+service.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // header头添加token
+  const token = getToken()
+  if (token) {
+    config.headers["token"] = token
+  }
+  return config;
+
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+service.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  // 导出订单数据返回类型是blob
+  return response.request.responseType == "blob" ? response.data : response.data.data;
+}, function (error) {
+  // 对响应错误做点什么
+  const msg = error.response.data.msg || "请求失败"
+  if (msg == "非法token，请先登录！") {
+    store.dispatch("logout").finally(() => { location.reload() })
+  }
+  toast(msg, "error")
+  return Promise.reject(error);
+});
+
+export default service
